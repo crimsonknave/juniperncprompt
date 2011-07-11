fcd12828e4e80c05f3df58538131755db0e1a205
Joseph Henrich <crimsonknave@gmail.com>
2011-07-11 16:50:25 -0400

    Added a GPL notice

--- juniperncprompt.py
+++ juniperncprompt.py
@@ -1,15 +1,29 @@
 #!/usr/bin/env python2
 
+###################
+# Copyright 2011 Joseph Henrich (jhenrich@constantcontact.com)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+######################
+
 import sys
 import getpass
 import argparse
 import urllib2, urllib, cookielib
 import subprocess
 import os
-
-if sys.version_info < (2,7):
-  print("Python 2.7 or greater is required")
-  sys.exit(0)
 
 
 # The defaults are what my set up expect, change them if you want
